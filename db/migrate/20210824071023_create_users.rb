bd2175b65c86a5baf707b88716c36ebf9d2ae360
tc-miyatani <miyahei38+tc@gmail.com>
2021-08-25 16:16:26 +0900

    usersテーブルに入室中フラグを追加

--- db/migrate/20210824071023_create_users.rb
+++ db/migrate/20210824071023_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string     :name, null: false
-      t.references :room, null: false, foreign_key: true
+      t.string     :name,     null: false
+      t.boolean    :is_enter, null: false, default: true
+      t.references :room,     null: false, foreign_key: true
 
       t.timestamps
     end
