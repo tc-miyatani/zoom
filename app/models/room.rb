bcd55d7f3691c7b2c298a05c8e8e52b08dac73c0
tc-miyatani <miyahei38+tc@gmail.com>
2021-08-25 12:03:15 +0900

    room, userモデルのバリデーション

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,21 @@
+require "securerandom"
 class Room < ApplicationRecord
   has_many :users, dependent: :destroy
+
+  with_options presence: true do
+    validates :title
+    validates :hashid, uniqueness: true
+  end
+  validates :is_private, inclusion: [true, false]
+
+  def save
+    generate_hashid
+    super
+  end
+
+  private
+
+  def generate_hashid
+    self.hashid = SecureRandom.urlsafe_base64
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   belogs_to :room
   has_many :messages
+
+  validates :name,  presence: true,
+                    uniqueness: {
+                      scope: :room_id,
+                      message: '同じ部屋に同じユーザー名の人が入室中です'
+                    }
+
 end
