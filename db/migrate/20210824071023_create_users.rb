0635dc47f56d083beab2b5e15617fd2416705db9
tc-miyatani <miyahei38+tc@gmail.com>
2021-08-26 01:46:40 +0900

    コメント

--- db/migrate/20210824071023_create_users.rb
+++ db/migrate/20210824071023_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string     :name,     null: false
-      t.boolean    :is_enter, null: false, default: true
+      t.boolean    :is_enter, null: false, default: true # tinyint
       t.references :room,     null: false, foreign_key: true
 
       t.timestamps
